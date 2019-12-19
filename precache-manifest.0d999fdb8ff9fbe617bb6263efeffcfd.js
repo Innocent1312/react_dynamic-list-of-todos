6f8291a74ba2acf683cfd5c6180f6d2bd1b9b0e9
Innocent1312 <anthonydev33@gmail.com>
2019-12-19 20:16:03 +0200

    Updates

---
+++ precache-manifest.0d999fdb8ff9fbe617bb6263efeffcfd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2feb09fdbef4f1fd6e875c6646d832",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "0a660b30eeb6f888d7e2",
+    "url": "/react_dynamic-list-of-todos/static/js/2.12db86b6.chunk.js"
+  },
+  {
+    "revision": "692a2ba939ad269b568d",
+    "url": "/react_dynamic-list-of-todos/static/js/main.6d12a430.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
